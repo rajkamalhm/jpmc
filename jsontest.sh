5406cbf0df87c6517474059e48be567f77b172d7
rajkamalhm <rajkamalhm@gmail.com>
2019-12-19 19:50:41 +0530

    Add files via upload

---
+++ jsontest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/dhruvbehl/jsonTest/master/login.log > ./login.log
+curl https://raw.githubusercontent.com/dhruvbehl/jsonTest/master/ipfilter.json > ./ipfilter.json
+
+for IP in $(cat ./ipfilter.json | jq -r '.data[].ipAddress')
+do
+	if [[ $(grep $IP ./login.log) ]]
+	then
+		grep $IP ./login.log | while read line
+		do
+			date_directory=$(echo $line | awk '{print $1"_" $2"_" $3}')
+			status=$(echo $line | cut -d" " -f 8 | sed 's/,//g')
+			mkdir -p ./output/${date_directory}
+			echo $line >> "./output/${date_directory}/${IP}_${status}.log"
+		done
+	fi
+done
